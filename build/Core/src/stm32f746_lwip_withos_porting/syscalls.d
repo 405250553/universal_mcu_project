de9b275c0b7a1ad3a2ae210207dec14e483f9b16
104020 <kuanhung.chen@senao.com>
2025-10-02 10:28:24 +0800

    remove generate file

--- build/Core/src/stm32f746_lwip_withos_porting/syscalls.d
+++
@@ -1,2 +0,0 @@
-build/Core/src/stm32f746_lwip_withos_porting/syscalls.o: \
- Core/src/stm32f746_lwip_withos_porting/syscalls.c
--- build/Core/src/stm32f746_lwip_withos_porting/sysmem.d
+++
@@ -1,2 +0,0 @@
-build/Core/src/stm32f746_lwip_withos_porting/sysmem.o: \
- Core/src/stm32f746_lwip_withos_porting/sysmem.c
