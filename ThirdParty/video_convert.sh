5fbdd53d373370defabd77fb777326b7f6d11f48
104020 <kuanhung.chen@senao.com>
2025-10-20 16:20:02 +0800

    video_convert.sh add bgr888 avi note: avi use bgr888 normally, although avi is a container format, but almost all video player can not recognize avi+rgb888

--- ThirdParty/video_convert.sh
+++ ThirdParty/video_convert.sh
@@ -1,78 +1,130 @@
 #!/bin/bash
-
 # 使用方式：
-# ./batch_convert.sh rgb565_raw [fps] [filename]
-# ./batch_convert.sh argb8888_raw [fps] [filename]
-# ./batch_convert.sh argb8888_avi [fps] [filename]
-# ./batch_convert.sh mjpeg [fps] [filename]
+# ./video_convert.sh [format] [fps] [file] [rotate90]
 
-FORMAT=$1
-USER_FPS=$2
-FILE=$3
-MAX_JOBS=4  # 同時轉檔數量，可依CPU核心數調整
+set -e
 
-if [ -z "$FORMAT" ]; then
-  echo "請指定輸出格式: rgb565_raw / argb8888_raw / argb8888_avi / mjpeg"
-  exit 1
+FORMAT="$1"
+USER_FPS=""
+FILE=""
+ROTATE=""
+
+# 輸出資料夾
+OUTDIR="output"
+
+# 判斷第二個參數
+if [[ "$2" == "rotate90" ]]; then
+    ROTATE="rotate90"
+elif [[ -n "$2" ]]; then
+    USER_FPS="$2"
 fi
 
-# 取得檔案列表
-if [ ! -z "$FILE" ]; then
-  FILES=("$FILE")
+# 判斷第三個參數
+if [[ "$3" == "rotate90" ]]; then
+    ROTATE="rotate90"
+elif [[ -n "$3" ]]; then
+    FILE="$3"
+fi
+
+# 第四個參數
+if [[ "$4" == "rotate90" ]]; then
+    ROTATE="rotate90"
+fi
+
+## TODO: rgb565_mov / rgb565_avi can not play
+## mov divide frame data to per mdat, avi is a complete frame
+# 檢查 format
+if [[ -z "$FORMAT" ]]; then
+    echo "請指定輸出格式:"
+    echo "  rgb565_mov / rgb565_avi / argb8888_avi / mjpeg_avi / mjpeg_mov"
+    echo "  bgr888_mov / bgr888_avi / rgb888_mov / rgb888_avi"
+    exit 1
+fi
+
+# 建立輸出資料夾
+mkdir -p "$OUTDIR"
+
+# 判斷檔案列表
+if [[ -z "$FILE" ]]; then
+    FILES=(*.MP4 *.mp4)
 else
-  FILES=(*.MP4)
+    FILES=("$FILE")
 fi
 
 for f in "${FILES[@]}"; do
-  (
+    [[ -f "$f" ]] || continue
     echo "開始轉檔：$f"
 
-    # 如果沒給 fps，使用 ffprobe 取得影片原始幀率
-    if [ -z "$USER_FPS" ]; then
-      RAW_FPS=$(ffprobe -v 0 -of csv=p=0 -select_streams v:0 -show_entries stream=r_frame_rate "$f")
-      # RAW_FPS 可能是 30000/1001 這種形式，需要計算
-      FPS=$(echo "scale=3; $RAW_FPS" | bc -l)
-      FPS_ARG="-r $FPS"
-    else
-      FPS_ARG="-r $USER_FPS"
+    FPS_ARG=()
+    if [[ -n "$USER_FPS" ]]; then
+        FPS_ARG=(-r "$USER_FPS")
+    fi
+
+    ROTATE_ARG=()
+    WIDTH=272
+    HEIGHT=480
+    BASENAME="$(basename "$f" | sed 's/\.[^.]*$//')"
+
+    if [[ "$ROTATE" == "rotate90" ]]; then
+        ROTATE_ARG=(-vf "transpose=2")
+        BASENAME="${BASENAME}_rotate90"
+        WIDTH=480
+        HEIGHT=272
     fi
 
     case "$FORMAT" in
-      rgb565_raw)
-        ffmpeg -i "$f" \
-          -f rawvideo -pix_fmt rgb565le $FPS_ARG -s 272x480 -an \
-          "${f%.MP4}_rgb565.raw"
-        ;;
-      argb8888_raw)
-        ffmpeg -i "$f" \
-          -f rawvideo -pix_fmt bgra $FPS_ARG -s 272x480 -an \
-          "${f%.MP4}_raw_argb8888.raw"
-        ;;
-      argb8888_avi)
-        ffmpeg -i "$f" \
-          -vcodec rawvideo -pix_fmt bgra $FPS_ARG -s 272x480 -an \
-          "${f%.MP4}_argb8888.avi"
-        ;;
-      mjpeg)
-        ffmpeg -i "$f" \
-          -vcodec mjpeg -q:v 3 $FPS_ARG -s 272x480 -pix_fmt yuvj422p \
-          -acodec pcm_u8 -ar 11025 \
-          "${f%.MP4}_mjpeg_audio.avi"
-        ;;
-      *)
-        echo "不支援的格式：$FORMAT"
-        exit 1
-        ;;
+        rgb565_mov)
+            ffmpeg -i "$f" "${FPS_ARG[@]}" "${ROTATE_ARG[@]}" -s ${WIDTH}x${HEIGHT} \
+                -vcodec rawvideo -pix_fmt rgb565le -acodec pcm_s16le -ar 44100 -ac 1 \
+                -f mov "${OUTDIR}/${BASENAME}_rgb565.mov"
+            ;;
+        rgb565_avi)
+            ffmpeg -i "$f" "${FPS_ARG[@]}" "${ROTATE_ARG[@]}" -s ${WIDTH}x${HEIGHT} \
+                -vcodec rawvideo -pix_fmt rgb565le -acodec pcm_s16le -ar 44100 -ac 1 \
+                -f avi "${OUTDIR}/${BASENAME}_rgb565.avi"
+            ;;
+        argb8888_avi)
+            ffmpeg -i "$f" "${FPS_ARG[@]}" "${ROTATE_ARG[@]}" -s ${WIDTH}x${HEIGHT} \
+                -vcodec rawvideo -pix_fmt bgra -acodec pcm_s16le -ar 44100 -ac 1 \
+                -f avi "${OUTDIR}/${BASENAME}_argb8888.avi"
+            ;;
+        mjpeg_avi)
+            ffmpeg -i "$f" "${FPS_ARG[@]}" "${ROTATE_ARG[@]}" -s ${WIDTH}x${HEIGHT} \
+                -vcodec mjpeg -pix_fmt yuvj422p -q:v 3 -acodec pcm_s16le -ar 44100 -ac 1 \
+                -f avi "${OUTDIR}/${BASENAME}_mjpeg.avi"
+            ;;
+        mjpeg_mov)
+            ffmpeg -i "$f" "${FPS_ARG[@]}" "${ROTATE_ARG[@]}" -s ${WIDTH}x${HEIGHT} \
+                -vcodec mjpeg -pix_fmt yuvj422p -q:v 3 -acodec pcm_s16le -ar 44100 -ac 1 \
+                -f mov "${OUTDIR}/${BASENAME}_mjpeg.mov"
+            ;;
+        bgr888_mov)
+            ffmpeg -i "$f" "${FPS_ARG[@]}" "${ROTATE_ARG[@]}" -s ${WIDTH}x${HEIGHT} \
+                -vcodec rawvideo -pix_fmt bgr24 -acodec pcm_s16le -ar 44100 -ac 1 \
+                -f mov "${OUTDIR}/${BASENAME}_bgr888.mov"
+            ;;
+        bgr888_avi)
+            ffmpeg -i "$f" "${FPS_ARG[@]}" "${ROTATE_ARG[@]}" -s ${WIDTH}x${HEIGHT} \
+                -vcodec rawvideo -pix_fmt bgr24 -acodec pcm_s16le -ar 44100 -ac 1 \
+                -f avi "${OUTDIR}/${BASENAME}_bgr888.avi"
+            ;;
+        rgb888_mov)
+            ffmpeg -i "$f" "${FPS_ARG[@]}" "${ROTATE_ARG[@]}" -s ${WIDTH}x${HEIGHT} \
+                -vcodec rawvideo -pix_fmt rgb24 -acodec pcm_s16le -ar 44100 -ac 1 \
+                -f mov "${OUTDIR}/${BASENAME}_rgb888.mov"
+            ;;
+        rgb888_avi)
+            ffmpeg -i "$f" "${FPS_ARG[@]}" "${ROTATE_ARG[@]}" -s ${WIDTH}x${HEIGHT} \
+                -vcodec rawvideo -pix_fmt rgb24 -acodec pcm_s16le -ar 44100 -ac 1 \
+                -f avi "${OUTDIR}/${BASENAME}_rgb888.avi"
+            ;;
+        *)
+            echo "不支援的格式：$FORMAT"
+            exit 1
+            ;;
     esac
 
     echo "完成：$f"
-  ) &
-
-  # 若背景任務數達上限，暫停直到有空位
-  while [ "$(jobs -r | wc -l)" -ge "$MAX_JOBS" ]; do
-    sleep 1
-  done
 done
 
-wait
-echo "全部轉檔完成！"
+echo "全部轉檔完成，輸出在資料夾：$OUTDIR"
